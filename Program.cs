cbfeff53bdbb3a1cc1801249700ceb1a077ad4f1
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2019-11-16 21:54:23 -0800

    More testing

--- Program.cs
+++ Program.cs
@@ -5,103 +5,106 @@ using Microsoft.AspNetCore.Server.Kestrel.Transport.Libuv.Internal;
 using Microsoft.AspNetCore.Server.Kestrel.Transport.Libuv.Internal.Networking;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Buffers;
 using System.IO.Pipelines;
 using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TestLibUV
 {
-    class Program
+  class Program
+  {
+    public static LibuvFunctions libUv = new LibuvFunctions();
+
+    public static string ByteArrayToString(byte[] ba) => BitConverter.ToString(ba).Replace("-", "");
+
+    static void Main(string[] args)
     {
-        public static LibuvFunctions libUv = new LibuvFunctions();
+      Console.WriteLine("Main: {0}", Thread.CurrentThread.ManagedThreadId);
 
-        public static string ByteArrayToString(byte[] ba) => BitConverter.ToString(ba).Replace("-", "");
-
-        static void Main(string[] args)
+      // Use the factory
+      LibuvTransportContext transport = new LibuvTransportContext
+      {
+        Options = new LibuvTransportOptions(),
+        AppLifetime = new ApplicationLifetime(null),
+        Log = new LibuvTrace(LoggerFactory.Create(builder =>
         {
-            UvLoopHandle loopHandle = new UvLoopHandle(null);
-            loopHandle.Init(libUv);
+          builder.AddConsole();
+        }).CreateLogger("core"))
+      };
 
-            Task.Run(async () =>
-            {
-                LibuvTransportContext transport = new LibuvTransportContext()
-                {
-                    Options = new LibuvTransportOptions(),
-                    AppLifetime = new ApplicationLifetime(null),
-                    Log = new LibuvTrace(LoggerFactory.Create(builder =>
-                    {
-                        builder.AddConsole();
-                    }).CreateLogger("core")),
-                };
-                LibuvConnectionListener listener = new LibuvConnectionListener(libUv, transport, new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2593));
-                Console.WriteLine("Binding...");
-                await listener.BindAsync();
+      LibuvThread uvThread = new LibuvThread(libUv, transport);
+      uvThread.StartAsync().Wait();
 
-                Console.WriteLine("Listening...");
-                ConnectionContext connectionContext = await listener.AcceptAsync();
-                Console.WriteLine("Accepted Connection from {0}", connectionContext.RemoteEndPoint);
-                PipeReader reader = connectionContext.Transport.Input;
+      Task.Run(async () =>
+      {
+        LibuvTransportContext transport = new LibuvTransportContext
+        {
+          Options = new LibuvTransportOptions(),
+          AppLifetime = new ApplicationLifetime(null),
+          Log = new LibuvTrace(LoggerFactory.Create(builder =>
+          {
+            builder.AddConsole();
+          }).CreateLogger("core"))
+        };
 
-                while (true)
-                {
-                    ReadResult readResult = await reader.ReadAsync();
-                    if (readResult.Buffer.IsSingleSegment)
-                        Console.WriteLine(ByteArrayToString(readResult.Buffer.First.ToArray()));
-                    else foreach (var segment in readResult.Buffer)
-                        Console.WriteLine(ByteArrayToString(segment.ToArray()));
-                }
-            });
+        LibuvConnectionListener listener = new LibuvConnectionListener(libUv, transport, new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2593));
+        Console.WriteLine("Binding...");
+        await listener.BindAsync();
 
-            Console.ReadLine();
+        Console.WriteLine("Listening...");
+        ConnectionContext connectionContext = await listener.AcceptAsync();
+        Console.WriteLine("Accepted Connection from {0}", connectionContext.RemoteEndPoint);
+        PipeReader reader = connectionContext.Transport.Input;
+
+        while (true)
+        {
+          ReadResult readResult = await reader.ReadAsync();
+          if (readResult.Buffer.IsSingleSegment)
+            Console.WriteLine(ByteArrayToString(readResult.Buffer.First.ToArray()));
+          else foreach (var segment in readResult.Buffer)
+              Console.WriteLine(ByteArrayToString(segment.ToArray()));
+        }
+      });
+
+      // Manually putting something on the queue from another thread (or the main thread)
+      uvThread.PostAsync<object>(_ =>
+      {
+        Console.WriteLine("Stuff ({0})", Thread.CurrentThread.ManagedThreadId);
+      }, null);
+
+      // Send an Initialization Request for Timers
+      uvThread.PostAsync<object>(_ =>
+      {
+        UvTimerHandle timerHandle = new UvTimerHandle(null);
+        timerHandle.Init(uvThread.Loop, (callback, handle) =>
+        {
+          Console.WriteLine("Closed");
+        });
+
+        Console.WriteLine("Timer Stuff {0}", Thread.CurrentThread.ManagedThreadId);
+        int count = 10;
+
+        void cb2(UvTimerHandle handle)
+        {
+          Console.WriteLine("Called!2 {0}", DateTime.Now);
+          timerHandle.Start(cb2, 2000, 0);
         }
 
-        static void Main2(string[] args)
+        void cb1(UvTimerHandle handle)
         {
-            UvLoopHandle loopHandle = new UvLoopHandle(null);
-            loopHandle.Init(libUv);
-
-            UvTimerHandle timerHandle = new UvTimerHandle(null);
-            timerHandle.Init(loopHandle, (callback, handle) =>
-            {
-                Console.WriteLine("Closed");
-            });
-
-            int count = 10;
-
-            void cb2(UvTimerHandle handle)
-            {
-                Console.WriteLine("Called!2 {0}", DateTime.Now);
-                timerHandle.Start(cb2, 2000, 0);
-            }
-
-            void cb1(UvTimerHandle handle)
-            {
-                Console.WriteLine("Called!1 {0}", DateTime.Now);
-                count--;
-                if (count < 0)
-                    timerHandle.Start(cb2, 2000, 0);
-                else
-                    timerHandle.Start(cb1, 1000, 0);
-            }
-
+          Console.WriteLine("Called!1 {0}", DateTime.Now);
+          count--;
+          if (count < 0)
+            timerHandle.Start(cb2, 2000, 0);
+          else
             timerHandle.Start(cb1, 1000, 0);
-
-            /*bool sw = true;*/
-
-            while (true)
-            {
-                loopHandle.Run(1);
-/*                if (sw && count >= 10)
-                {
-                    Console.WriteLine("Switching!");
-                    sw = false;
-                    timerHandle.Start((handle) =>
-                    {
-                        Console.WriteLine("Called!2");
-                    }, 10000, 10000);
-                }*/
-            }
         }
+
+        timerHandle.Start(cb1, 1000, 0);
+      }, null);
+
+      Console.ReadLine();
     }
+  }
 }
